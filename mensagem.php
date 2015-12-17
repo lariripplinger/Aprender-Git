e115e86e3efe564f23ceb99bf5bd7953d814dff4
Lari José Ripplinger <lari@onehost.com.br>
2015-12-17 18:23:10 -0200

    alteração da função 1821

--- mensagem.php
+++ mensagem.php
@@ -1,7 +1,7 @@
 <?php
 // Script: teste github ////
 // Desenvolvedor: Lari Ripplinger ////
-// Data: 17/12/2015 ////
+// Data: 17/Dezembro/2015 mudei mes de 12 para dezembro////
 // //////////////////////////////////////////////////////////////////////////////////////
-// 
+// isso adicionado na linha de comentario 6
 ?>
