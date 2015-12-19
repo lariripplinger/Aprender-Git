0c00622a2fe9454b8dbf6bf922ce8983c700a157
Lari José Ripplinger <lari@onehost.com.br>
2015-12-19 08:53:47 -0200

    Update mensagem.php

--- mensagem.php
+++ mensagem.php
@@ -1,7 +1,8 @@
 <?php
 // Script: teste github ////
-// Desenvolvedor: Lari Ripplinger ////
-// Data: 17/Dezembro/2015 mudei mes de 12 para dezembro////
+// Desenvolvedor: Lari Jose Ripplinger ////
+// Data: 19/Dezembro/2015 mudei mes de 12 para dezembro////
 // //////////////////////////////////////////////////////////////////////////////////////
 // isso adicionado na linha de comentario 6
+// aqui vou escrever o que fiz, alterei na lina 3 coloquei Jose, na linha 4 alterei para 19 e inclui essa linah com descrições 
 ?>
